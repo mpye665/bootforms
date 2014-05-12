905f7f8ff24d77c34aad32143708f404bfd3093b
mpye665 <mpye665@cse.unsw.edu.au>
2014-05-12 15:47:51 +1000

    Inline radio list

--- src/AdamWathan/BootForms/BasicFormBuilder.php
+++ src/AdamWathan/BootForms/BasicFormBuilder.php
@@ -5,6 +5,7 @@ use AdamWathan\BootForms\Elements\FormGroup;
 use AdamWathan\BootForms\Elements\CheckGroup;
 use AdamWathan\BootForms\Elements\HelpBlock;
 use AdamWathan\BootForms\Elements\GroupWrapper;
+use AdamWathan\BootForms\Elements\RadioList;
 
 class BasicFormBuilder
 {
@@ -23,7 +24,7 @@ class BasicFormBuilder
 		$formGroup = new FormGroup($label, $control);
 
 		if ($this->builder->hasError($name)) {
-			$formGroup->helpBlock($this->builder->getError($name));
+			$formGroup->helpBlock(new HelpBlock($this->builder->getError($name)));
 			$formGroup->addClass('has-error');
 		}
 
@@ -76,14 +77,18 @@ class BasicFormBuilder
 
 	protected function buildCheckGroup($label, $name, $control)
 	{
-		$label = $this->builder->label($label, $name)->after($control)->addClass('control-label');
-
-		$checkGroup = new CheckGroup($label);
-
+		$checkGroup = $this->buildCheckGroupWithoutError($label, $name, $control);
 		if ($this->builder->hasError($name)) {
-			$checkGroup->helpBlock($this->builder->getError($name));
+			$checkGroup->helpBlock(new HelpBlock($this->builder->getError($name)));
 			$checkGroup->addClass('has-error');
 		}
+		return $checkGroup;
+	}
+
+	protected function buildCheckGroupWithoutError($label, $name, $control)
+	{
+		$label = $this->builder->label($label, $name)->after($control)->addClass('control-label');
+		$checkGroup = new CheckGroup($label);
 		return $checkGroup;
 	}
 
@@ -92,15 +97,50 @@ class BasicFormBuilder
 		if (is_null($value)) {
 			$value = $label;
 		}
+		if (is_array($value)) {
+			return $this->createRadioListGroup($label, $name, $value);
+		}
 
 		$control = $this->builder->radio($name, $value);
 
 		return $this->radioGroup($label, $name, $control);
 	}
 
+	protected function createRadioListGroup($title, $name, $options)
+	{
+		$label = $this->builder->label($title, $name)->addClass('control-label');
+		$control = $this->buildRadioList($name, $options);
+		$formGroup = new FormGroup($label, $control);
+		if ($this->builder->hasError($name)) {
+			$formGroup->helpBlock(new HelpBlock($this->builder->getError($name)));
+			$formGroup->addClass('has-error');
+		}
+		return $this->wrap($formGroup);
+	}
+
+	protected function buildRadioList($name, $options)
+	{
+		$radios = [];
+		if (! is_assoc($options)) {
+			$options = array_combine(array_values($options), array_values($options));
+		}
+		foreach ($options as $value => $label) {
+			$control = $this->builder->radio($name, $value);
+			$group = $this->radioGroupWithoutError($label, $name, $control);
+			$radios[] = $group;
+		}
+		return new RadioList($radios);
+	}
+
 	protected function radioGroup($label, $name, $control)
 	{
 		$checkGroup = $this->buildCheckGroup($label, $name, $control);
+		return $this->wrap($checkGroup->addClass('radio'));
+	}
+
+	protected function radioGroupWithoutError($label, $name, $control)
+	{
+		$checkGroup = $this->buildCheckGroupWithoutError($label, $name, $control);
 		return $this->wrap($checkGroup->addClass('radio'));
 	}
 
@@ -151,7 +191,7 @@ class BasicFormBuilder
 		$formGroup = new FormGroup($label, $control);
 
 		if ($this->builder->hasError($name)) {
-			$formGroup->helpBlock($this->builder->getError($name));
+			$formGroup->helpBlock(new HelpBlock($this->builder->getError($name)));
 			$formGroup->addClass('has-error');
 		}
 
@@ -161,40 +201,5 @@ class BasicFormBuilder
 	public function __call($method, $parameters)
 	{
 		return call_user_func_array(array($this->builder, $method), $parameters);
-	}
-
-
-
-
-
-	// Application specific functions.
-
-	protected function textWithPlaceholder($label, $name, $value = null, $placeholder = null)
-	{
-		$control = $this->builder->text($name)->value($value);
-		if (isset($placeholder)) $control->placeholder($placeholder);
-
-		return $this->formGroup($label, $name, $control);	
-	}
-
-	public function homePhone($label, $name, $value = null)
-	{
-		return $this->textWithPlaceholder($label, $name, $value, 'E.g. (02)98553958');
-	}
-
-	public function mobile($label, $name, $value = null)
-	{
-		return $this->textWithPlaceholder($label, $name, $value, 'E.g. 0454309486');
-	}
-
-	public function gender($label, $name)
-	{
-
-	}
-
-	public function datePicker($label, $name, $value = null)
-	{
-		$control = $this->builder->datePicker($name)->value($value);	
-		return $this->formGroup($label, $name, $control);
 	}
 }
